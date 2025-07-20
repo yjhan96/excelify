027c889b050f9490b50375f436583e1b9e9e07d0
Albert Han <yjhan96@gmail.com>
2025-07-19 22:13:40 -0400

    modified simple example.

--- examples/simple/df.py
+++ examples/simple/df.py
@@ -3,18 +3,18 @@ import excelify as el
 length = 20
 
 df = el.ExcelFrame.empty(
-    columns=["year", "annual_return", "compounded_amount", "annual_investment"],
+    columns=["year", "annual_return", "compounded_amount", "annual_investment", "total_return"],
     height=length,
 )
 df = df.with_columns(
     year=el.lit([i + 1 for i in range(length)]),
-    annual_return=el.Map(
+    annual_return=el.map(
         lambda idx: 0.15 if idx == 0 else el.col("annual_return").prev(1)
     ),
-    annual_investment=el.Map(
+    annual_investment=el.map(
         lambda idx: 120.0 if idx == 0 else el.col("annual_investment").prev(1)
     ),
-    compounded_amount=el.Map(
+    compounded_amount=el.map(
         lambda idx: 100.0
         if idx == 0
         else (
@@ -22,9 +22,12 @@ df = df.with_columns(
             + el.col("annual_investment")
         )
     ),
+    total_return=el.map(
+        lambda idx: (el.col("compounded_amount") - el.cell(df["compounded_amount"][0])) / el.cell(df["compounded_amount"][0])
+    ),
 )
 
-df = df.select(["year", "annual_investment", "annual_return", "compounded_amount"])
+df = df.select(["year", "annual_investment", "annual_return", "compounded_amount", "total_return"])
 
 
 # TODO: Should this go under style?
@@ -34,18 +37,9 @@ df["compounded_amount"][0].is_editable = True
 (
     df.style.fmt_integer(columns=["year"])
     .fmt_currency(columns=["annual_investment", "compounded_amount"], accounting=True)
-    .fmt_percent(columns=["annual_return"])
-    .value_color(columns=["annual_investment"], color="green")
+    .fmt_percent(columns=["annual_return", "total_return"])
 )
 
-df2 = el.ExcelFrame.empty(columns=["x", "y"], height=10)
-df2 = df2.with_columns(
-    x=el.col("annual_investment", from_=df), y=el.col("compounded_amount", from_=df)
-)
+sheet_styler = el.SheetStyler().cols_width({"B": 150, "C": 110, "D": 150, "E": 120})
 
-(df2.style.fmt_integer(columns=["y"]))
-
-
-sheet_styler = el.SheetStyler().cols_width({"B": 150, "C": 110, "D": 150})
-
-el.display([(df, (0, 0)), (df2, (0, len(df.columns)))], sheet_styler=sheet_styler)
+el.display([(df, (0, 0))], sheet_styler=sheet_styler)
